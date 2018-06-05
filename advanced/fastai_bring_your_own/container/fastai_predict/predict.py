583fc38d5baf12e7cf2e881402019b29efb3bbc8
Matt McClean <mmcclean@amazon.com>
2018-06-05 16:48:51 +0000

    updated predict.py

--- advanced/fastai_bring_your_own/container/fastai_predict/predict.py
+++ advanced/fastai_bring_your_own/container/fastai_predict/predict.py
@@ -1,12 +1,14 @@
 import os, json, traceback
 import urllib.parse
 
+import ast
+import json
+
+import boto3
+import torch
+import dill as dill 
 import numpy as np
 
-# Do not remove the following line
-import sys;sys.path.append("..")  # NOQA
-
-import model
 import utils
 
 _MODEL_PATH = os.path.join('/opt/ml/', 'model')  # Path where all your model(s) live in
@@ -14,6 +16,59 @@ _TMP_IMG_PATH = os.path.join('/tmp', 'images')
 _TMP_IMG_FILE = os.path.join(_TMP_IMG_PATH, 'image.jpg')
 
 IMG_SIZE = int(os.environ.get('IMAGE_SIZE', '224'))
+
+class ClassificationService:
+    class __ClassificationService:
+        
+        def __init__(self, model_path):
+            self.model_path = model_path
+            self._model = None
+            self._classes = None
+        
+        def __str__(self):
+            return repr(self) + self.model_path
+            
+        @property
+        def model(self):
+            if not self._model:
+                # Get the model filename
+                model_file = utils.get_file_with_ext(self.model_path, '.pt')
+                print(f'Model file is: {model_file}')
+                
+                self._model = torch.load(model_file, map_location='cpu', pickle_module=dill).cpu()
+                print("Created model successfully")
+            return self._model
+    
+        @property
+        def classes(self):
+            if not self._classes:
+                classes_file = utils.get_file_with_ext(self.model_path, '.json')
+                print(f'Classes file is: {classes_file}')
+
+                with open(classes_file) as f:
+                    self._classes = ast.literal_eval(json.load(f))
+            return self._classes
+    
+    instance = None
+    
+    def __init__(self, model_path):
+        if not ClassificationService.instance:
+            ClassificationService.instance = ClassificationService.__ClassificationService(model_path)
+        else:
+            ClassificationService.instance.model_path = model_path
+            ClassificationService.instance._model = None
+            ClassificationService.instance._classes = None
+    
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+    
+    @property
+    def model(self):
+        return self.instance.model
+        
+    @property
+    def classes(self):
+        return self.instance.classes
 
 print("Creating predictor object")
 predictor = ClassificationService(_MODEL_PATH)
@@ -26,12 +81,12 @@ def predict(img_bytes):
     """
     
     print("Got new request")
-    write_test_image(img_bytes, _TMP_IMG_PATH, _TMP_IMG_FILE)
+    utils.write_test_image(img_bytes, _TMP_IMG_PATH, _TMP_IMG_FILE)
     
     print("Opening test image")
-    test_img = open_image(_TMP_IMG_FILE)
+    test_img = utils.open_image(_TMP_IMG_FILE)
     print("Pre-processing test image")
-    p_img = preproc_img(test_img, IMG_SIZE)
+    p_img = utils.preproc_img(test_img, IMG_SIZE)
     
     print("Calling model")
     log_preds = predictor.model(p_img).data.numpy()
