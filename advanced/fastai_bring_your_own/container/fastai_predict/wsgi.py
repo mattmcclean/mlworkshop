16e041ee9045b2b77792aff1c04af23e901de02c
Matt McClean <mmcclean@amazon.com>
2018-06-05 16:05:06 +0000

    updated wsgi file

--- advanced/fastai_bring_your_own/container/fastai_predict/wsgi.py
+++ advanced/fastai_bring_your_own/container/fastai_predict/wsgi.py
@@ -1,4 +1,4 @@
-from . import predictor as myapp
+import predictor as myapp
 
 # This is just a simple wrapper for gunicorn to find your app.
 # If you want to change the algorithm file, simply change "predictor" above to the
